18d584d92cb2ec24c69679b77a338a2639f4ecae
Jin <jinxuguang@xinxindai.com>
2018-03-08 18:19:50 +0800

    commit

--- app/src/main/java/com/proxydemo/administrator/pullrefresh/MainActivity.java
+++ app/src/main/java/com/proxydemo/administrator/pullrefresh/MainActivity.java
@@ -1,10 +1,12 @@
 package com.proxydemo.administrator.pullrefresh;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.proxydemo.administrator.pullrefresh.wight.PtrFrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +14,21 @@ public class MainActivity extends AppCompatActivity {
     private   String[] strs = new String[] {
         "first", "second", "third", "fourth", "fifth"
     };//定义一个String数组用来显示ListView的内容
+    private PtrFrameLayout prt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mListview = findViewById(R.id.lv);
+        prt = findViewById(R.id.ptr);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prt.setLoadingEnd();
+            }
+        });
+
         mListview.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, strs));
 
--- app/src/main/java/com/proxydemo/administrator/pullrefresh/wight/PtrFrameLayout.java
+++ app/src/main/java/com/proxydemo/administrator/pullrefresh/wight/PtrFrameLayout.java
@@ -2,10 +2,12 @@ package com.proxydemo.administrator.pullrefresh.wight;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 import android.widget.TextView;
 
 /**
@@ -14,21 +16,30 @@ import android.widget.TextView;
 
 public class PtrFrameLayout extends ViewGroup {
 
+    private static final String TAG = PtrFrameLayout.class.getSimpleName();
     private View mHeaderView;
-    private int mHeaderId=0;
+    private int mHeaderId = 0;
     protected View mContent;
     private int mContainerId = 0;
     private int mHeaderHeight;
     private float mLastX;
     private float mLastY;
     private MotionEvent mLastMoveEvent;
+    private float mResistance = 1.7f;
+    private float mOffsetX;
+    private float mOffsetY;
+    private float mCurrentPos = 0;
+    private float mLastFlingY = 0;
+    private Scroller mScroller;
+    private boolean mIsMove = false;
+
 
     public PtrFrameLayout(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public PtrFrameLayout(Context context, AttributeSet attrs) {
-        this(context, attrs,-1);
+        this(context, attrs, -1);
     }
 
     public PtrFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -37,19 +48,19 @@ public class PtrFrameLayout extends ViewGroup {
     }
 
     private void initView() {
-
+        mScroller = new Scroller(getContext());
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         if (mHeaderView != null) {
-            measureChild(mHeaderView,widthMeasureSpec,heightMeasureSpec);
+            measureChild(mHeaderView, widthMeasureSpec, heightMeasureSpec);
             mHeaderHeight = mHeaderView.getMeasuredHeight();
         }
 
         if (mContent != null) {
-            measureChild(mContent,widthMeasureSpec,heightMeasureSpec);
+            measureChild(mContent, widthMeasureSpec, heightMeasureSpec);
         }
     }
 
@@ -70,11 +81,11 @@ public class PtrFrameLayout extends ViewGroup {
             mHeaderView.layout(left, top, right, bottom);
         }
         if (mContent != null) {
-             int contentLeft = paddingLeft;
-             int contentTop = paddingTop;
-             int contentRight = contentLeft+mContent.getMeasuredWidth();
-             int contentBottom = contentTop+mContent.getMeasuredHeight();
-            mContent.layout(contentLeft,contentTop,contentRight,contentBottom);
+            int contentLeft = paddingLeft;
+            int contentTop = paddingTop;
+            int contentRight = contentLeft + mContent.getMeasuredWidth();
+            int contentBottom = contentTop + mContent.getMeasuredHeight();
+            mContent.layout(contentLeft, contentTop, contentRight, contentBottom);
         }
     }
 
@@ -142,19 +153,58 @@ public class PtrFrameLayout extends ViewGroup {
                 break;
             case MotionEvent.ACTION_MOVE:
                 mLastMoveEvent = ev;
-                float offsetX = ev.getX() - mLastX;
-                float offsetY = ev.getY() - mLastY;
+                mIsMove = true;
+                Log.i(TAG, "ACTION_MOVE" + "    ev.getX()=" + ev.getY() + "mLastY=" + mLastY);
+                mOffsetX = ev.getX() - mLastX;
+                mOffsetY = ev.getY() - mLastY;
+                mOffsetY = mOffsetY / mResistance;
+
+//                Log.i(TAG, "ACTION_MOVE" + " ------------------------------------------------");
                 mLastX = ev.getX();
                 mLastY = ev.getY();
-                mHeaderView.offsetTopAndBottom((int) offsetY);
-                mContent.offsetTopAndBottom((int) offsetY);
+                mCurrentPos = mCurrentPos + mOffsetY;
+//                Log.i(TAG, "ACTION_MOVE" + "    mCurrentPos=" + mCurrentPos + "mOffsetY=" + mOffsetY);
+                mHeaderView.offsetTopAndBottom((int) mOffsetY);
+                mContent.offsetTopAndBottom((int) mOffsetY);
                 break;
             case MotionEvent.ACTION_UP:
-                break;
             case MotionEvent.ACTION_CANCEL:
+                mIsMove = false;
+                Log.i(TAG, "ACTION_CANCEL" + "    mCurrentPos=" + mCurrentPos);
+                if (mCurrentPos - mHeaderHeight > 0) {
+                    mCurrentPos = mCurrentPos - mHeaderHeight;
+                }
+                mScroller.startScroll(0, 0, 0, (int) mCurrentPos, 200);
+                invalidate();
+                break;
+            default:
                 break;
         }
         return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void computeScroll() {
+        boolean finish = !mScroller.computeScrollOffset() || mScroller.isFinished();
+        int currY = mScroller.getCurrY();
+        float deltaY = currY - mLastFlingY;
+        Log.i(TAG, "currY=" + currY + "      deltaY=: " + deltaY);
+        if (!finish) {
+            mLastFlingY = currY;
+            mHeaderView.offsetTopAndBottom(-(int) deltaY);
+            mContent.offsetTopAndBottom(-(int) deltaY);
+            invalidate();
+        } else {
+//            if (!mIsMove) {
+//                mLastFlingY = 0;
+//                mCurrentPos = 0;
+//                Log.i(TAG, "computeScroll Finish" + "    mCurrentPos=" + mCurrentPos);
+//            }
+            mLastFlingY = 0;
+            mCurrentPos = 0;
+            Log.i(TAG, "computeScroll Finish" + "    mCurrentPos=" + mCurrentPos);
+
+        }
     }
 
     public void setHeaderView(View view) {
@@ -169,5 +219,23 @@ public class PtrFrameLayout extends ViewGroup {
         }
         addView(mHeaderView);
         //TODO 不需要重新绘制？
+    }
+
+    public void setLoadingEnd() {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                mScroller.startScroll(0, 0, 0, (int) mHeaderHeight, 100);
+                invalidate();
+            }
+        });
+    }
+
+    public float getResistance() {
+        return mResistance;
+    }
+
+    public void setResistance(float resistance) {
+        mResistance = resistance;
     }
 }
